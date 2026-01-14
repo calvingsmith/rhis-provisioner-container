0e8f83820da543a89c5cdaaa69d835c5aa533896
casmith@redhat.com <casmith@redhat.com>
2026-01-14 13:19:35 +0000

    made readable

--- sources/build_idm_primary.sh
+++ sources/build_idm_primary.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ansible-playbook -i /rhis/vars/external_inventory/inventory -e "vault_dir=/rhis/vars/vault" -u ansiblerunner --ask-pass --ask-vault-pass /rhis/rhis-builder-idm/main.yml
+ansible-playbook -i /rhis/vars/external_inventory/inventory \
+            -e "vault_dir=/rhis/vars/vault" \
+            -u ansiblerunner --ask-pass --ask-vault-pass \
+            /rhis/rhis-builder-idm/main.yml
