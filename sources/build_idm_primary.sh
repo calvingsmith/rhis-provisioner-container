322e8402eb0f593727b971ed730358de0902f4e7
casmith@redhat.com <casmith@redhat.com>
2026-01-15 06:24:07 +0000

    builder updates

--- sources/build_idm_primary.sh
+++ sources/build_idm_primary.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ansible-playbook -i /rhis/vars/external_inventory/inventory \
-            -e "vault_dir=/rhis/vars/vault" \
-            -u ansiblerunner --ask-pass --ask-vault-pass \
-            /rhis/rhis-builder-idm/main.yml
+        -e "vault_dir=/rhis/vars/vault" \
+        -u ansiblerunner \
+        --ask-pass --ask-vault-pass \
+        /rhis/rhis-builder-idm/phase1_setup.yml
