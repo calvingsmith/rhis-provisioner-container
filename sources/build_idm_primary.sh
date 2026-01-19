caf7503b34e8ee8eb51103176383276937c31980
casmith@redhat.com <casmith@redhat.com>
2026-01-19 19:45:59 +0000

    Update build_idm_primary script and version to 1.1

--- sources/build_idm_primary.sh
+++ sources/build_idm_primary.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ansible-playbook -i /rhis/vars/external_inventory/inventory \
-        -e "vault_dir=/rhis/vars/vault" \
-        -u ansiblerunner \
-        --ask-vault-pass \
+        -e "vault_path=/rhis/vars/vault/rhis_builder_vault.yml" \
+        --ssh-common-args "-o Identityfile=/rhis/vars/vault/ssh_priv_key"  \
+        --vault-password-file /rhis/vars/vault/vault_key -v \
         /rhis/rhis-builder-idm/phase1_setup.yml
