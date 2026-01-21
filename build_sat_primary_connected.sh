b97ae921bd43b5115fb4e2aa2dc7b2f582517f52
casmith@redhat.com <casmith@redhat.com>
2026-01-21 21:22:08 +0000

    Refactor ansible-playbook command in build_sat_primary_connected.sh for improved vault handling

--- build_sat_primary_connected.sh
+++ build_sat_primary_connected.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-ansible-playbook    -i /rhis/vars/external_inventory/inventory \
-                    -e "vault_dir=/rhis/vars/vault" \
-                    -u ansiblerunner \
-                    --ask-pass --ask-vault-pass --limit=sat_primary \
-                    /rhis/rhis-builder-satellite/main.yml
+ansible-playbook   -i /rhis/vars/external_inventory/inventory \
+                   -e "vault_path=/rhis/vars/vault/rhis_builder_vault.yml" \
+                   --ssh-common-args "-o Identityfile=/rhis/vars/vault/ssh_priv_key"  \
+                   --vault-password-file /rhis/vars/vault/vault_key -v \
+                   --limit=sat_primary \
+                   /rhis/rhis-builder-satellite/main.yml
